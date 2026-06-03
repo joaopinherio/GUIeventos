f12a6f2ea87e3a00c54cf3a0a45448d2b7d12e71
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-06-03 23:38:45 +0000

    dev start Formulario

--- exemplo/src/main/java/br/pucrs/nomeusuario/exemplo/FormularioMen.java
+++ exemplo/src/main/java/br/pucrs/nomeusuario/exemplo/FormularioMen.java
@@ -3,10 +3,22 @@ package br.pucrs.nomeusuario.exemplo;
 import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-public class FormularioMen {
+@PageTitle("Formulario de mensagens")
+@Route("/forms")
+public class FormularioMen extends VerticalLayout{
+    private Button enviar;
+    private Button terminar;
+    private TextField mensagem;
+    private TextField mensagens;
+
+    public FormularioMen(){
+        
+    }
+
 
 }
